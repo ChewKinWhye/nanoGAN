ca7133ab0df23a6caf68538328a283e0c64aeaca
ChewKinWhye <e0200920@u.nus.edu>
2020-07-06 07:17:59 +0000

    KDFJ

--- tuning_pipeline.py
+++ tuning_pipeline.py
@@ -1,5 +1,5 @@
 import os
-os.system("CUDA_VISIBLE_DEVICES=1 python main.py --epochs 100 --alpha 0.8 --beta 20 --gamma 1 --pretrain 0 --d_lr 1e-4 --g_lr 1e-4  --latent_dim 50 --data_size 1000000 --data_path /hdd/modifications/ecoli/deepsignal/ --batch_size 512 --output_filename 001")
+os.system("CUDA_VISIBLE_DEVICES=-1 python main.py --epochs 100 --alpha 1 --beta 0 --gamma 1 --pretrain 0 --d_lr 1e-4 --g_lr 1e-4  --latent_dim 80 --data_size 1000000 --data_path /hdd/modifications/ecoli/deepsignal/ --batch_size 256 --output_filename 001")
 
 #os.system("CUDA_VISIBLE_DEVICES=1 python main.py --epochs 50 --alpha 0.8 --beta 20 --gamma 1 --pretrain 20 --d_lr 1e-4 --g_lr 2e-4  --latent_dim 80 --data_size 200000 --data_path /hdd/modifications/ecoli/deepsignal/ --batch_size 512 --output_filename 002")
 
