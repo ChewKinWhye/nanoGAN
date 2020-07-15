cd687d5fc7ef1a9adf329d9b1290386dea9cdcb2
ChewKinWhye <Ckinwhye@hotmail.com>
2020-07-16 06:14:22 +0800

    Fixed VAE bugs

--- VAE.py
+++ VAE.py
@@ -1,15 +1,12 @@
-import numpy as np
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 import matplotlib.pyplot as plt
-from sklearn import preprocessing
-
+import numpy as np
 from utils.data import load_data
 from utils.arguments import parse_args
-from utils.evaluate import compute_metrics, plot_prc
-from sklearn.metrics import auc, precision_recall_curve, roc_curve, f1_score, accuracy_score
-from sklearn.utils import class_weight
+from utils.evaluate import compute_metrics_standardized
+import random
 
 class Sampling(layers.Layer):
     """Uses (z_mean, z_log_var) to sample z, the vector encoding a digit."""
@@ -21,9 +18,9 @@ class Sampling(layers.Layer):
         epsilon = tf.keras.backend.random_normal(shape=(batch, dim))
         return z_mean + tf.exp(0.5 * z_log_var) * epsilon
 
-latent_dim = 20
+latent_dim = 2
 
-encoder_inputs = keras.Input(shape=(428,))
+encoder_inputs = keras.Input(shape=(479,))
 x = layers.Dense(256, activation="relu")(encoder_inputs)
 x = layers.Dense(64, activation="relu")(x)
 x = layers.Dense(32, activation="relu")(x)
@@ -37,7 +34,8 @@ latent_inputs = keras.Input(shape=(latent_dim,))
 x = layers.Dense(32, activation="relu")(latent_inputs)
 x = layers.Dense(64, activation="relu")(x)
 x = layers.Dense(256, activation="relu")(x)
-decoder_outputs = layers.Dense(428)(x)
+x = layers.Dense(479, activation="sigmoid")(x)
+decoder_outputs = layers.Reshape((479,))(x)
 decoder = keras.Model(latent_inputs, decoder_outputs, name="decoder")
 decoder.summary()
 
@@ -57,7 +55,7 @@ class VAE(keras.Model):
             reconstruction_loss = tf.reduce_mean(
                 keras.losses.binary_crossentropy(data, reconstruction)
             )
-            reconstruction_loss *= 428
+            reconstruction_loss *= 479
             kl_loss = 1 + z_log_var - tf.square(z_mean) - tf.exp(z_log_var)
             kl_loss = tf.reduce_mean(kl_loss)
             kl_loss *= -0.5
@@ -73,9 +71,13 @@ class VAE(keras.Model):
 
 def plot_label_clusters(encoder, decoder, data, labels):
     # display a 2D plot of the digit classes in the latent space
-    z_mean, _, _ = encoder.predict(data)
+    filter_indices = random.sample(range(0, data.shape[0]-1), 3000)
+    data_sample = np.take(data, filter_indices, axis=0)
+    print(labels[:])
+    label_sample = np.take(labels, filter_indices, axis=0)
+    z_mean, _, _ = encoder.predict(data_sample)
     plt.figure(figsize=(12, 10))
-    plt.scatter(z_mean[:, 0], z_mean[:, 1], c=1-labels, s=0.5)
+    plt.scatter(z_mean[:, 0], z_mean[:, 1], c=label_sample, s=0.5)
     plt.colorbar()
     plt.xlabel("z[0]")
     plt.ylabel("z[1]")
@@ -83,51 +85,42 @@ def plot_label_clusters(encoder, decoder, data, labels):
 
 
 args = parse_args()
-x_train, x_test, y_test, x_val, y_val = load_data(args)
-print(list(y_val).count(1))
-print(list(y_val).count(0))
-
-normalized_x_test = preprocessing.normalize(x_test)
-normalized_x_val = preprocessing.normalize(x_val)
+_, x_train, y_train, x_test, y_test = load_data(args)
 
 vae = VAE(encoder, decoder)
 vae.compile(optimizer=keras.optimizers.Adam())
-vae.fit(normalized_x_test, epochs=100, batch_size=128)
-plot_label_clusters(encoder, decoder, normalized_x_test, y_test)
+vae.fit(x_train, epochs=50, batch_size=128)
+plot_label_clusters(encoder, decoder, x_train, y_train)
 
 
 predictor_input = keras.Input(shape=(latent_dim,))
 x = layers.Dense(8, activation="relu")(predictor_input)
 x = layers.Dense(16, activation="relu")(x)
-x = layers.Dense(32, activation="relu")(x)
-predictor_output = layers.Dense(1, activation="relu")(x)
+x = layers.Dense(8, activation="relu")(x)
+predictor_output = layers.Dense(1, activation="sigmoid")(x)
 predictor = keras.Model(predictor_input, predictor_output, name="predictor")
-
-
-w = {0: 1.,
-     1: 9.}
 
 predictor.compile(loss=keras.losses.binary_crossentropy, optimizer=keras.optimizers.Adam())
 
-x_train_predictor = encoder.predict(normalized_x_val)
-predictor.fit(x_train_predictor, y_val, epochs=100, batch_size=128, class_weight=w)
+print(x_train.shape)
+x_train_mean, x_train_sd, _ = encoder.predict(x_train)
+x_train = np.concatenate((x_train_mean, x_train_sd), axis=1)
+print(x_train.shape)
+predictor.fit(x_train_mean, y_train, epochs=10, batch_size=128)
 
 # Test model
-enc_out = encoder.predict(normalized_x_val)
-pred_out = predictor.predict(enc_out)
+x_test_mean, x_test_sd, _ = encoder.predict(x_test)
+x_test = np.concatenate((x_test_mean, x_test_sd), axis=1)
 
-precision, recall, _ = precision_recall_curve(y_val, pred_out)
-fpr, tpr, _ = roc_curve(y_val, pred_out)
+pred_out = predictor.predict(x_test_mean)
+accuracy_val, sensitivity_val, specificity_val, precision_val, au_roc_val, cm_val = compute_metrics_standardized(
+    pred_out, y_test)
 
-plt.plot(recall, precision, 'r-', label="Precision-Recall curve of model")
-plt.plot(fpr, tpr, 'b-', label="FPR-TPR curve of model")
-random_predictions = np.random.random_sample((len(y_val)))
-rand_au_prc, rand_recall, rand_precision, rand_au_roc, rand_fpr, rand_tpr, rand_accuracy, rand_f_measure \
-        = compute_metrics(random_predictions, y_val, 0.5)
-plt.plot(rand_recall, rand_precision, 'r:', label="Precision-Recall curve of random")
-plt.plot(rand_fpr, rand_tpr, 'b:', label="FPR-TPR curve of random")
-plt.xlabel('Recall/FPR')
-plt.ylabel('Precision/TPR')
-plt.axis([0, 1, 0, 1])
-plt.legend()
-plt.show()
+print(f"\tAccuracy    : {accuracy_val:.3f}")
+print(f"\tSensitivity : {sensitivity_val:.3f}")
+print(f"\tSpecificity : {specificity_val:.3f}")
+print(f"\tPrecision   : {precision_val:.3f}")
+print(f"\tAUC         : {au_roc_val:.3f}")
+print(f"{cm_val}")
+
+
